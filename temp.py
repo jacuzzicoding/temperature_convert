7708486db9fcbb6f9c7adba80deef75111648be8
Brock Jenkinson <brockjenkinson@gmail.com>
2024-05-16 03:04:23 -0400

    feat: Add welcome message to temperature converter

---
+++ temp.py
@@ -0,0 +1,7 @@
+#This is a temperature converter written in Python
+welcome1 = "Welcome to Brock's Temperature Converter! " # Welcome message to the user
+welcome2 = "What scale are you converting from? (CHOOSE C/F)"
+print(welcome1+welcome2)
+
+
+
