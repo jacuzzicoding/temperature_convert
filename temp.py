d445abb053b0a8611c89a51d68aaa98ff06bf038
Brock Jenkinson <brockjenkinson@gmail.com>
2024-05-16 03:25:07 -0400

    feat: Add temperature conversion from Celsius to Fahrenheit

--- temp.py
+++ temp.py
@@ -1,7 +1,16 @@
 #This is a temperature converter written in Python
 welcome1 = "Welcome to Brock's Temperature Converter! " # Welcome message to the user
 welcome2 = "What scale are you converting from? (CHOOSE C/F)"
-print(welcome1+welcome2)
+print(welcome1+welcome2) 
+input1 = input() # User input for the scale they are converting from. 
+#using if statements to determine the scale the user is converting from
+if input1 == "C": # If the user chooses Celsius
+        print("You are converting from Celsius.")
+        print("What is the temperature in Celsius?")
+        temp1 = float(input()) # User input for the temperature in Celsius
+        temp2 = (temp1 * 9/5) + 32 # Conversion formula from Celsius to Fahrenheit
+        print("The temperature in Fahrenheit is", temp2) # Output of the temperature in Fahrenheit
+
 
 
 
